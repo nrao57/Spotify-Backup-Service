e5a23aa50711f3c0a244e64b252429d4c9c42e34
nrao57 <raonikhil57@gmail.com>
2022-09-18 21:05:31 +0000

    working backups to json

--- backup.py
+++ backup.py
@@ -5,24 +5,23 @@ import base64
 
 
 def base64_encode(client_id, client_secret):
-    return base64.b64encode(f"{client_id}:{client_secret}".encode('utf-8'))
+    return base64.b64encode(f"{client_id}:{client_secret}".encode('utf-8')).decode("utf-8") 
 
 def get_access_token():
     url = "https://accounts.spotify.com/api/token"
-
+    creds = base64_encode(os.environ['SPOTIFY_CLIENT_ID'], os.environ['SPOTIFY_CLIENT_SECRET'])
     payload='grant_type=client_credentials'
     headers = {
-        'Authorization': 'Basic ' + base64_encode(os.environ['SPOTIFY_CLIENT_ID'], os.environ['SPOTIFY_CLIENT_SECRET']),
+        'Authorization': f'Basic {creds}',
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()['access_token']
 
 def get_access_token_headers(access_token):
     return {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + access_token
+        'Authorization': f'Bearer {access_token}'
     }
 
 
@@ -40,7 +39,18 @@ def get_playlist_data(access_token_headers, playlist_id, next=None):
     
     response = requests.request("GET", url, headers=access_token_headers, data={})
     data = response.json()
-    return data, data['tracks']['next']
+    if data:
+        if data.get('tracks', {}).get('items'):
+            return data['tracks']['items'], data['tracks']['next']
+        else:
+            return data['items'], data['next']
+    else:
+        print(f"no data returned for playlist_id {playlist_id}")
+
+
+def write_data_to_file(filename, data_dict):
+    with open(filename, 'w') as f:
+     json.dump(data_dict, f)
 
 
 all_data = {}
@@ -48,11 +58,12 @@ all_data = {}
 token_header = get_access_token_headers(get_access_token())
 playlists_data = get_user_playlist(access_token_headers=get_access_token_headers(token_header))
 for playlist in playlists_data['items']:
+    print(f"Getting tracks for playlist: {playlist['name']}")
     next = None
     while(True):
-        playlist_data, next = get_playlist_data( get_access_token_headers(token_header), playlist['id'], next)
-        all_data[playlist['name']] = playlist_data
+        playlist_tracks, next = get_playlist_data( get_access_token_headers(token_header), playlist['id'], next)
+        all_data[playlist['name']] = playlist_tracks
         if not next:
             break
     
-print(all_data)
+write_data_to_file("playlist_backup.json", all_data)
